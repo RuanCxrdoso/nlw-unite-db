feebb9e44c52f73e4565a0a8b65228fe53921f1b
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-05-10 23:37:19 -0300

    fix: server

--- src/server.ts
+++ src/server.ts
@@ -50,6 +50,6 @@ app.setErrorHandler(errorHandler)
 
 const PORT = process.env.PORT || 3333; // Use a porta atribuída pelo ambiente ou 3333 como padrão
 
-app.listen(PORT, () => {
-  console.log(`HTTP server running on port ${PORT}`);
-});
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running!')
+})
